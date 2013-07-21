81834c43681e8f1999b7b957fbc686ca6badc971
forhappy <haipingf@gmail.com>
2013-07-21 23:22:47 +0800

    add LIS nlog(n) solution.

--- oj/DP/LIS/LIS.cc
+++ oj/DP/LIS/LIS.cc
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
 int LIS(const std::vector<int> arr) {
-    for_each(arr.begin(), arr.end(), [](int i){ std::cout << i << "\t"; });
+    std::for_each(arr.begin(), arr.end(), [](int i) {
+        std::cout << i << "\t";
+    });
     std::cout << std::endl;
     std::vector<int> aux(arr.size(), 0);
     std::vector<int>::const_iterator it;
@@ -23,9 +27,55 @@ int LIS(const std::vector<int> arr) {
         }
         aux[curr] = aux[curr_max_pos] + 1;
     }
-    for_each(aux.begin(), aux.end(), [](int i){ std::cout << i << "\t"; });
+    std::for_each(aux.begin(), aux.end(), [](int i) {
+        std::cout << i << "\t";
+    });
     std::cout << std::endl;
     return *std::max_element(aux.begin(), aux.end());
+}
+
+// binary search.
+static int aux_bsearch(int A[], int l, int r, int key) {
+    int m;
+
+    while( r - l > 1 ) {
+        m = l + (r - l)/2;
+        (A[m] >= key ? r : l) = m;
+    }
+
+    return r;
+}
+
+int LIS(int A[], int size) {
+    // add boundary case, when array size is one
+
+    int *tailTable   = new int[size];
+    int len; // always points empty slot
+
+    memset(tailTable, 0, sizeof(tailTable[0])*size);
+
+    tailTable[0] = A[0];
+    len = 1;
+    for( int i = 1; i < size; i++ ) {
+        if( A[i] < tailTable[0] )
+            // new smallest value
+            tailTable[0] = A[i];
+        else if( A[i] > tailTable[len-1] )
+            // A[i] wants to extend largest subsequence
+            tailTable[len++] = A[i];
+        else
+            // A[i] wants to be current end candidate of an existing subsequence
+            // it will replace ceil value in tailTable
+            tailTable[CeilIndex(tailTable, -1, len-1, A[i])] = A[i];
+    }
+
+    std::for_each(tailTable, tailTable + size, [](int i) {
+        std::cout << i << "\t";
+    });
+    std::cout << std::endl;
+    delete[] tailTable;
+
+    return len;
 }
 
 /*
@@ -38,8 +88,10 @@ int main(int argc, const char *argv[])
 {
 
     // std::vector<int> a = {1, 3, 9, 8, 4, 5, 6, 2, 7, 8, 3, 1, 2};
-    std::vector<int> a = {1, 2, 3, 1, 4, 5, 3};
-    int result = LIS(a);
+    // std::vector<int> a = {1, 2, 3, 1, 4, 5, 3};
+    // int a[] = {1, 2, 3, 1, 4, 5, 3};
+    int a[] = {1, 2, 3, 1, 4, 5, 3, 6, 8, 7, 3};
+    int result = LIS(a, sizeof(a) / sizeof(int));
 
     std::cout << "LIS: " << result << std::endl;
 
