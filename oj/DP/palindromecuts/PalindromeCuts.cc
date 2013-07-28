1c20d660bdea1fad6dce3d1b4c7c3d8aaeb410ab
forhappy <haipingf@gmail.com>
2013-07-29 00:11:40 +0800

    add PalindromeCuts problem, need to be implemented.

---
+++ oj/DP/palindromecuts/PalindromeCuts.cc
@@ -0,0 +1,26 @@
+// Given a string, a cut of the string is a palindrome partitioning
+// if every substring of the partition is a palindrome.
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int PalindromeCuts(const std::string& src) {
+
+}
+
+
+
+/*
+ * ===  FUNCTION  =========================================================
+ *         Name:  main
+ *  Description:  program entry routine.
+ * ========================================================================
+ */
+int main(int argc, const char *argv[])
+{
+    return EXIT_SUCCESS;
+}  /* ----------  end of function main  ---------- */
