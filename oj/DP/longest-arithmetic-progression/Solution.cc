1936137f9fc46c7028192f33cc646ab8a1af0cb2
forhappy <haipingf@gmail.com>
2013-07-30 22:48:14 +0800

    add longest arithmetic progression problem.

---
+++ oj/DP/longest-arithmetic-progression/Solution.cc
@@ -0,0 +1,29 @@
+// Given a set of numbers, find the Length of the
+// Longest Arithmetic Progression (LLAP) in it.
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int LongestArithmeticProgression(const std::vector<int>& arr) {
+    assert(arr.size() > 0);
+
+    if (arr.size() == 1) return 1;
+    // TODO: need implementation here.
+
+}
+
+/*
+ * ===  FUNCTION  =========================================================
+ *         Name:  main
+ *  Description:  program entry routine.
+ * ========================================================================
+ */
+int main(int argc, const char *argv[])
+{
+    return EXIT_SUCCESS;
+}  /* ----------  end of function main  ---------- */
