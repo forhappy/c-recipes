c1e3d6694b093d5e9ec07bb9807b529e5e4527f8
forhappy <haipingf@gmail.com>
2013-08-01 12:44:47 +0800

    add min jumps problem.

---
+++ oj/DP/min-jumps/Solution.cc
@@ -0,0 +1,25 @@
+// Given an array of integers where each element represents the max number
+// of steps that can be made forward from that element. Write a function
+// to return the minimum number of jumps to reach the end of the array
+// (starting from the first element). If an element is 0, then cannot move
+// through that element.
+//
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+/*
+ * ===  FUNCTION  =========================================================
+ *         Name:  main
+ *  Description:  program entry routine.
+ * ========================================================================
+ */
+int main(int argc, const char *argv[])
+{
+    return EXIT_SUCCESS;
+}  /* ----------  end of function main  ---------- */
