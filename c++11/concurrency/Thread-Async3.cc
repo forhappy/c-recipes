67925d82274d3672cb855a5960a3cde2e1a8e842
forhappy <haipingf@gmail.com>
2013-08-04 16:54:00 +0800

    add Thread-Async3.cc

---
+++ c++11/concurrency/Thread-Async3.cc
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cmath>
+#include <future>
+#include <iostream>
+
+std::string ThreadTask(std::string const& s) {
+    return s;
+}
+
+/*
+ * ===  FUNCTION  =========================================================
+ *         Name:  main
+ *  Description:  program entry routine.
+ * ========================================================================
+ */
+int main(int argc, const char *argv[])
+{
+    std::string s = "hello";
+    // std::future<std::string> f(std::async(
+    //             std::launch::deferred, [s]{return ThreadTask(s);}));
+
+    std::future<std::string> f(std::async(
+                std::launch::deferred, [&s]{return ThreadTask(s);}));
+
+    s = "byebye, ";
+
+    std::cout << s << std::endl;
+    std::cout << f.get() << " world" << std::endl;
+
+    return EXIT_SUCCESS;
+}  /* ----------  end of function main  ---------- */
