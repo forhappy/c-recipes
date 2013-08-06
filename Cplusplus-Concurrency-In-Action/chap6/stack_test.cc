deaad7c8a2a5597e576aa34559b3e1d686ce4db7
forhappy <haipingf@gmail.com>
2013-08-06 10:30:59 +0800

    add multi-thread testcase.

--- Cplusplus-Concurrency-In-Action/chap6/stack_test.cc
+++ Cplusplus-Concurrency-In-Action/chap6/stack_test.cc
@@ -18,9 +18,25 @@
 
 #include <iostream>
 #include <memory>
+#include <thread>
 
 #include "stack.h"
 #include "gtest/gtest.h"
+
+static const int kStackOperations = 100;
+
+static void stack_push_task(ccia::Stack<int>& stack) {
+    for (int i = 0; i < kStackOperations; i++) {
+        stack.push(i);
+    }
+}
+
+static void stack_pop_task(ccia::Stack<int>& stack) {
+    for (int i = 0; i < kStackOperations; i++) {
+        stack.pop();
+    }
+}
+
 
 TEST(Stack, SingleThreadNormal) {
     ccia::Stack<int> stack;
@@ -38,4 +54,16 @@ TEST(Stack, SingleThreadNormal) {
     EXPECT_EQ(2, value);
     stack.pop(value);
     EXPECT_EQ(1, value);
+}
+
+TEST(Stack, MultiThreadNormal) {
+    ccia::Stack<int> stack;
+    std::thread t1(stack_push_task, std::ref(stack));
+    std::thread t2(stack_push_task, std::ref(stack));
+    t1.join();
+    t2.join();
+    std::thread t3(stack_pop_task, std::ref(stack));
+    std::thread t4(stack_pop_task, std::ref(stack));
+    t3.join();
+    t4.join();
 }
