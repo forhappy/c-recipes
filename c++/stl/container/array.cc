a50a93a5e2547c1fe924e3403ac5821a876308c5
forhappy <haipingf@gmail.com>
2013-08-10 08:04:40 +0800

    update std::array example.

--- c++/stl/container/array.cc
+++ c++/stl/container/array.cc
@@ -30,6 +30,7 @@ int main(int argc, const char *argv[])
 {
     // Initialization.
     std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::array<int, 10> arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 
     // Iterator.
@@ -55,6 +56,7 @@ int main(int argc, const char *argv[])
     std::cout << "array max_size: " << arr.max_size() << std::endl;
     std::cout << "array empty?: " << arr.empty() << std::endl;
 
+
     // Element access
     std::cout << arr[1] << arr[2] << arr[3] << std::endl;
     arr[1] = 111;
@@ -62,6 +64,20 @@ int main(int argc, const char *argv[])
     arr[3] = 111;
     std::cout << arr[1] << arr[2] << arr[3] << std::endl;
     std::cout << "array front: " << arr.front() << " array back: " << arr.back() << std::endl;
+    std::cout << *(arr.data() + 2) << std::endl;
+
+
+    // Modifiers
+    arr.fill(2113);
+    std::for_each(arr.cbegin(), arr.cend(),
+            [] (int i) {std::cout << i << "\t";});
+    std::cout << std::endl;
+
+    arr.swap(arr2);
+    std::for_each(arr.cbegin(), arr.cend(),
+            [] (int i) {std::cout << i << "\t";});
+    std::cout << std::endl;
+
 
     return EXIT_SUCCESS;
 }  /* ----------  end of function main  ---------- */
