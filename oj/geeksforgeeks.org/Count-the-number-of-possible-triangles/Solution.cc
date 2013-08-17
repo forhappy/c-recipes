d118e2dd27dbc65b6571280ff6dc26803daccd20
forhappy <haipingf@gmail.com>
2013-08-17 16:57:03 +0800

    add Count-the-number-of-possible-triangles

---
+++ oj/geeksforgeeks.org/Count-the-number-of-possible-triangles/Solution.cc
@@ -0,0 +1,20 @@
+// Description here: XXX
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+/*
+ * ===  FUNCTION  =========================================================
+ *         Name:  main
+ *  Description:  program entry routine.
+ * ========================================================================
+ */
+int main(int argc, const char *argv[])
+{
+    return EXIT_SUCCESS;
+}  /* ----------  end of function main  ---------- */
