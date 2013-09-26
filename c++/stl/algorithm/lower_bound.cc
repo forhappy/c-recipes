ba68946968f3aa7611a1c1addb6ae8c089fc1d1a
forhappy <haipingf@gmail.com>
2013-09-26 22:45:31 +0800

    udpate lower_bound implementation.

--- c++/stl/algorithm/lower_bound.cc
+++ c++/stl/algorithm/lower_bound.cc
@@ -1,7 +1,28 @@
 // lower_bound/upper_bound example
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <iterator>
 #include <vector>
+
+template <typename ForwardIterator, class T>
+ForwardIterator my_lower_bound(ForwardIterator first,
+        ForwardIterator last, const T& value)
+{
+    ForwardIterator it;
+    typename std::iterator_traits<ForwardIterator>::difference_type count, step;
+    count = std::distance(first, last);
+    while (count > 0) {
+        it = first;
+        step = count / 2;
+        std::advance(it, step);
+        if (*it < value) { // or: if (comp(*it,value)), for the comp version
+            first = ++it;
+            count -= step + 1;
+        }
+        else count = step;
+    }
+    return first;
+}
 
 int main () {
   int myints[] = {10,20,30,30,20,10,10,20};
